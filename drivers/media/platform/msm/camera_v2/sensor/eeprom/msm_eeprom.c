aa93158908698cb2d2ff8de9c6f1c37c3ab7cc67
Davide Garberi <dade.garberi@gmail.com>
2020-06-07 07:02:25 +0700

    drivers: camera_v2: Import xiaomi changes
    * Remove useless ifdef config other devices by @fiqri19102002
    
    Change-Id: Ied3e6b342e448f3835b5a61101fc5fc23367f197
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
+++ drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -25,13 +25,15 @@ DEFINE_MSM_MUTEX(msm_eeprom_mutex);
 #ifdef CONFIG_COMPAT
 static struct v4l2_file_operations msm_eeprom_v4l2_subdev_fops;
 #endif
-
-/*
- * msm_get_read_mem_size - Get the total size for allocation
- * @eeprom_map_array:	mem map
- *
- * Returns size after computation size, returns error in case of error
- */
+#ifdef CONFIG_MACH_LONGCHEER
+struct vendor_eeprom s_vendor_eeprom[CAMERA_VENDOR_EEPROM_COUNT_MAX];
+#endif
+/**
+  * msm_get_read_mem_size - Get the total size for allocation
+  * @eeprom_map_array:	mem map
+  *
+  * Returns size after computation size, returns error in case of error
+  */
 static int msm_get_read_mem_size
 	(struct msm_eeprom_memory_map_array *eeprom_map_array)
 {
@@ -227,15 +229,16 @@ static int read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl,
 	}
 	return rc;
 }
-/*
- * msm_eeprom_parse_memory_map() - parse memory map in device node
- * @of:	device node
- * @data:	memory block for output
- *
- * This functions parses @of to fill @data.  It allocates map itself, parses
- * the @of node, calculate total data length, and allocates required buffer.
- * It only fills the map, but does not perform actual reading.
- */
+
+/**
+  * msm_eeprom_parse_memory_map() - parse memory map in device node
+  * @of:	device node
+  * @data:	memory block for output
+  *
+  * This functions parses @of to fill @data.  It allocates map itself, parses
+  * the @of node, calculate total data length, and allocates required buffer.
+  * It only fills the map, but does not perform actual reading.
+  */
 static int msm_eeprom_parse_memory_map(struct device_node *of,
 	struct msm_eeprom_memory_block_t *data)
 {
@@ -659,11 +662,16 @@ static int msm_eeprom_config(struct msm_eeprom_ctrl_t *e_ctrl,
 		CDBG("%s E CFG_EEPROM_GET_MM_INFO\n", __func__);
 		rc = msm_eeprom_get_cmm_data(e_ctrl, cdata);
 		break;
+
 	case CFG_EEPROM_INIT:
 		if (e_ctrl->userspace_probe == 0) {
 			pr_err("%s:%d Eeprom already probed at kernel boot",
 				__func__, __LINE__);
-			rc = -EINVAL;
+#ifdef CONFIG_MACH_LONGCHEER
+                        rc = 0;
+#else
+                        rc = -EINVAL;
+#endif
 			break;
 		}
 		if (e_ctrl->cal_data.num_data == 0) {
@@ -1632,11 +1640,16 @@ static int msm_eeprom_config32(struct msm_eeprom_ctrl_t *e_ctrl,
 		CDBG("%s E CFG_EEPROM_READ_CAL_DATA\n", __func__);
 		rc = eeprom_config_read_cal_data32(e_ctrl, argp);
 		break;
+
 	case CFG_EEPROM_INIT:
 		if (e_ctrl->userspace_probe == 0) {
 			pr_err("%s:%d Eeprom already probed at kernel boot",
 				__func__, __LINE__);
+#ifdef CONFIG_MACH_LONGCHEER
+			rc = 0;
+#else
 			rc = -EINVAL;
+#endif
 			break;
 		}
 		if (e_ctrl->cal_data.num_data == 0) {
@@ -1693,7 +1706,109 @@ static long msm_eeprom_subdev_fops_ioctl32(struct file *file, unsigned int cmd,
 }
 
 #endif
+#ifdef CONFIG_MACH_LONGCHEER
+#ifdef CONFIG_MACH_XIAOMI_WHYRED
+static camera_vendor_module_id whyred_s5k5e8_ofilm_i_get_otp_vendor_module_id
+	(struct msm_eeprom_ctrl_t *e_ctrl)
+{
+	uint8_t INFO_NUM = 16;
+	uint8_t AWB_NUM = 8;
+	uint8_t LENSID_OFFSET = 9;
+	uint8_t SENSOR_OFFSET = 13;
+	uint8_t MODULE_OFFSET = 2;
+	uint8_t GROUP_MEMBER_NUM = 1 + INFO_NUM + AWB_NUM + 1;
 
+	uint32_t LSC_NUM = 360;
+	uint32_t FLAG_OFFSET ;
+
+	uint8_t mid=0;
+	uint8_t lensid=0;
+	uint8_t sensorid=0;
+	uint8_t flag=0;
+	uint8_t *buffer = e_ctrl->cal_data.mapdata;
+	int8_t i;
+
+	for(i = 2; i >= 0; --i){
+		FLAG_OFFSET = LSC_NUM + i*GROUP_MEMBER_NUM;
+		printk("i=%d flag offset %d\n",i,FLAG_OFFSET);
+		flag = buffer[FLAG_OFFSET];
+		if(0x55 != flag){
+			mid = MID_NULL;
+			pr_err("Invalid flag = 0x%x\n",flag);
+			continue;
+		}
+		lensid = buffer[FLAG_OFFSET + LENSID_OFFSET];
+		mid = buffer[FLAG_OFFSET + MODULE_OFFSET];
+		sensorid = buffer[FLAG_OFFSET + SENSOR_OFFSET];
+		printk("%s flag = 0x%x, mid = 0x%x, lensid = 0x%x, sensorid = 0x%x \n", __func__, flag, mid, lensid, sensorid);
+		break;
+	}
+	if((MID_OFILM != mid) || (SENSOR_S5K5E8 != sensorid))
+		mid = MID_NULL;
+	return mid;
+}
+
+static camera_vendor_module_id whyred_s5k5e8_qtech_ii_get_otp_vendor_module_id
+	(struct msm_eeprom_ctrl_t *e_ctrl)
+{
+	uint8_t INFO_NUM = 16;
+	uint8_t AWB_NUM = 8;
+	uint8_t LENSID_OFFSET = 9;
+	uint8_t SENSOR_OFFSET = 13;
+	uint8_t MODULE_OFFSET = 2;
+	uint8_t GROUP_MEMBER_NUM = 1 + INFO_NUM + AWB_NUM + 1;
+
+	uint32_t LSC_NUM = 360;
+	uint32_t FLAG_OFFSET ;
+
+	uint8_t mid=0;
+	uint8_t lensid=0;
+	uint8_t sensorid=0;
+	uint8_t flag=0;
+	uint8_t *buffer = e_ctrl->cal_data.mapdata;
+	int8_t i;
+
+	for(i = 2; i >= 0; --i){
+		FLAG_OFFSET = LSC_NUM + i*GROUP_MEMBER_NUM;
+		printk("i=%d flag offset %d\n",i,FLAG_OFFSET);
+		flag = buffer[FLAG_OFFSET];
+		if(0x55 != flag){
+			mid = MID_NULL;
+			pr_err("Invalid flag = 0x%x\n",flag);
+			continue;
+		}
+		lensid = buffer[FLAG_OFFSET + LENSID_OFFSET];
+		mid = buffer[FLAG_OFFSET + MODULE_OFFSET];
+		sensorid = buffer[FLAG_OFFSET + SENSOR_OFFSET];
+		printk("%s flag = 0x%x, mid = 0x%x, lensid = 0x%x, sensorid = 0x%x \n", __func__, flag, mid, lensid, sensorid);
+		break;
+	}
+
+	if((MID_QTECH != mid) || (SENSOR_S5K5E8 != sensorid))
+		mid = MID_NULL;
+	return mid;
+}
+
+#endif
+
+static uint8_t get_otp_vendor_module_id(struct msm_eeprom_ctrl_t *e_ctrl, const char *eeprom_name)
+{
+	camera_vendor_module_id module_id=MID_NULL;
+
+#ifdef CONFIG_MACH_XIAOMI_WHYRED
+	if (strcmp(eeprom_name, "whyred_s5k5e8_ofilm_i") == 0) {
+		module_id = whyred_s5k5e8_ofilm_i_get_otp_vendor_module_id(e_ctrl);
+	} else if (strcmp(eeprom_name, "whyred_s5k5e8_qtech_ii") == 0) {
+		module_id = whyred_s5k5e8_qtech_ii_get_otp_vendor_module_id(e_ctrl);
+	}
+#endif
+
+	if(module_id>=MID_MAX) module_id = MID_NULL;
+    	printk("%s %d eeprom_name=%s, module_id=0x%x\n",__func__,__LINE__, 
+			eeprom_name, module_id);
+	return ((uint8_t)module_id);
+}
+#endif
 static int msm_eeprom_platform_probe(struct platform_device *pdev)
 {
 	int rc = 0;
@@ -1838,7 +1953,12 @@ static int msm_eeprom_platform_probe(struct platform_device *pdev)
 		for (j = 0; j < e_ctrl->cal_data.num_data; j++)
 			CDBG("memory_data[%d] = 0x%X\n", j,
 				e_ctrl->cal_data.mapdata[j]);
-
+#ifdef CONFIG_MACH_LONGCHEER
+		if(eb_info->eeprom_name != NULL){
+			s_vendor_eeprom[pdev->id].module_id = get_otp_vendor_module_id(e_ctrl, eb_info->eeprom_name);
+			strcpy(s_vendor_eeprom[pdev->id].eeprom_name, eb_info->eeprom_name);
+		}
+#endif
 		e_ctrl->is_supported |= msm_eeprom_match_crc(&e_ctrl->cal_data);
 
 		rc = msm_camera_power_down(power_info,
